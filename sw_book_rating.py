ad5dbd44f593475fc0f2801004582c394c61fb46
backfromsolaris <porraszach@gmail.com>
2021-10-18 13:42:49 -0500

    converted to OOP via Class

--- sw_book_rating.py
+++ sw_book_rating.py
@@ -1,38 +1,49 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.keys import Keys
-import time
-
-service = Service(r"C:\Users\porra\operator\misc\installs\chromedriver.exe")
-
-driver = webdriver.Chrome(service=service)
-
-# access Youtini search page
-driver.get("https://www.youtini.com/search")
 
 
-# What book are you looking for?
-book = "Master and Apprentice"
+class BookDetails:
+    def __init__(self, book):
+        self.book = book
+        self.service = Service(r"C:\Users\porra\operator\misc\installs\chromedriver.exe")
+        self.driver = webdriver.Chrome(service=self.service)
 
-# search for book using search form
-search_1 = driver.find_element_by_id("search")
-search_1.send_keys(f"{book} complete book details")
-search_1.send_keys(Keys.RETURN)
+    def book_test(self):
+        return self.book
 
-driver.implicitly_wait(3)
+    def get_book(self):
+        driver = self.driver
 
-# access first search result
-search_2 = driver.find_element_by_class_name("link")
+        # access Youtini search page
+        self.driver.get("https://www.youtini.com/search")
 
-# route to href attribute
-book_href = search_2.get_attribute("href")
-driver.get(f'{book_href}')
+        # search for book using search form
+        search_1 = self.driver.find_element_by_id("search")
+        search_1.send_keys(f"{self.book} complete book details")
+        search_1.send_keys(Keys.RETURN)
 
-# locates review score
-search_3 = driver.find_element_by_class_name("review-score-prof.header")
-score = driver.execute_script("return arguments[0].innerHTML;", search_3)
-print(f'The score for {book} is ' + score)
+        # slight pause for html to load
+        driver.implicitly_wait(0.5)
 
-# time.sleep(5)
+        # access first search result
+        search_2 = driver.find_element_by_class_name("link")
 
-driver.quit()
+        # route to href attribute i.e. book page
+        book_href = search_2.get_attribute("href")
+        driver.get(f'{book_href}')
+
+    def book_score(self):
+        self.get_book()
+        driver = self.driver
+
+        # locates review score
+        search_3 = driver.find_element_by_class_name("review-score-prof.header")
+        score = driver.execute_script("return arguments[0].innerHTML;", search_3)
+        driver.quit()
+        return f'The score for {self.book} is ' + score
+
+        
+chosen_book = "Lost Stars"
+my_deets = BookDetails(chosen_book)
+print(my_deets.book_score())
