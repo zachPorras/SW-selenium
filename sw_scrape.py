9df61d482230cf8e36ce44c0e7cad3aa3db7b71e
backfromsolaris <porraszach@gmail.com>
2021-10-20 14:06:31 -0500

    get_character in-progress

--- sw_scrape.py
+++ sw_scrape.py
@@ -28,7 +28,21 @@ class CharacterDetails:
         self.character = character
 
     def get_character(self):
-        pass
+        # access Youtini search page
+        driver.get("https://starwars.fandom.com/wiki/Special:Search?query=&scope=internal&navigationSearch=true")
+
+        # search for book using search form
+        search_1 = driver.find_element(By.ID, 'search')
+        search_1.send_keys(f"{self.book} complete book details")
+        search_1.send_keys(Keys.RETURN)
+
+        # access first search result (first instance of .link)
+        search_2 = driver.find_element(By.CLASS_NAME, 'link')
+
+        # route to href attribute i.e. book page
+        book_href = search_2.get_attribute("href")
+        driver.get(f'{book_href}')
+        # driver.quit()
 
     def character_img(self):
         pass
